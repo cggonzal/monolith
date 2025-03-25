ccd69bafd64cb49e1654298e7165b9e8d4abe4b7
cggonzal <gonzalezcgg1@gmail.com>
2025-03-24 21:42:35 -0700

    expose db handle using function

--- db/db.go
+++ db/db.go
@@ -10,18 +10,22 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var dbHandle *gorm.DB
+
+func GetDB() *gorm.DB {
+	return dbHandle
+}
 
 // Connect initializes the database connection
 func Connect() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{}) // Change to postgres.Open(...) for PostgreSQL
+	dbHandle, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{}) // Change to postgres.Open(...) for PostgreSQL
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
 
 	// Auto-migrate all registered models
-	DB.AutoMigrate(
+	dbHandle.AutoMigrate(
 		&models.User{},
 		&models.Job{},
 	)
