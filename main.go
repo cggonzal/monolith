79796646f2821be733f69e28dce646c9bd8f089f
Carlos Gonzalez <gonzalezcgg1@gmail.com>
2025-06-12 19:35:09 -0700

    Add basic model generator

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"monolith/db"
+	"monolith/generator"
 	"monolith/handlers"
 	"monolith/server_management"
 	"monolith/ws"
@@ -19,6 +20,14 @@ var staticFiles embed.FS
 var templateFiles embed.FS
 
 func main() {
+	// Dispatch to generators if requested
+	if len(os.Args) > 1 && os.Args[1] == "generator" {
+		if err := generator.Run(os.Args[2:]); err != nil {
+			slog.Error("generator failed", "error", err)
+		}
+		return
+	}
+
 	// Configure global structured logging
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
