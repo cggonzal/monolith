0082abfec94be3c972158b7caec041d167fc0722
cggonzal <gonzalezcgg1@gmail.com>
2025-06-14 10:33:46 -0700

    remove unneeded struct

--- email/email.go
+++ email/email.go
@@ -7,13 +7,6 @@ import (
 	"monolith/models"
 )
 
-type payload struct {
-	Subject string   `json:"subject"`
-	Body    string   `json:"body"`
-	Sender  string   `json:"sender"`
-	To      []string `json:"to"`
-}
-
 // SendEmail enqueues an email job using the global job queue.
 // subject and body are the email contents. sender is the "from" address.
 // recipients is the list of destination email addresses.
