48c7c501e808d3d6ba8b43e34117010f22baf814
cggonzal <gonzalezcgg1@gmail.com>
2025-07-04 09:39:37 -0700

    move email directory from top level into services directory

--- services/services.go
+++
@@ -1,5 +0,0 @@
-/*
-Package services contains business logic helpers that can be called from
-controllers or jobs. Each service ideally lives in its own file.
-*/
-package services
--- services/services_test.go
+++
@@ -1,5 +0,0 @@
-package services
-
-import "testing"
-
-func TestPlaceholder(t *testing.T) {}
